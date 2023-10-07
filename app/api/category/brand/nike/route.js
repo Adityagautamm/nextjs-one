80e9bd5225f710d8e112f7db6da0b75a26b0acea
Adityagautamm <adityagautamm@gmail.com>
2023-10-07 22:45:28 +1000

    Mongo db connection working

--- app/api/category/brand/nike/route.js
+++ app/api/category/brand/nike/route.js
@@ -2,11 +2,28 @@
 //import Prompt from "@models/prompt";
 //import { connectToDB } from "@utils/database";
 
-export const GET = async (request) => {
-  try {
-    // await connectToDB();
+import { connectToDb, getDb } from "../../../../../db/db";
 
-    //const prompts = await Prompt.find({}).populate("creator");
+export const GET = async (request) => {
+  let db;
+  try {
+    //   // await connectToDB();
+    await connectToDb((err) => {
+      if (!err) {
+        console.log("DB connection is working");
+        db = getDb();
+      } else {
+        console.log("DB not working");
+      }
+    });
+
+    //   db.collection("sneakerDetails")
+    //     .find()
+    //     .then(() => {})
+    //     .catch(() => {
+    //       console.log("Mongo db data connect issue");
+    //     });
+
     const data = [
       { name: "one", price: "$50", image: "one.webp" },
       { name: "two", price: "$40", image: "one.webp" },
@@ -20,6 +37,7 @@ export const GET = async (request) => {
       { name: "ten", price: "$80", image: "one.webp" },
     ];
 
+    // net ninja video 17 for mongo db, great way to do response
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch the brands data", { status: 500 });
